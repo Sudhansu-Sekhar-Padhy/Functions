ae63a593f29fefc1306db2056431369019279871
Sudhansu-Sekhar-Padhy <sidsekhar809@gmail.com>
2022-10-15 14:27:29 +0530

    1 Function

---
+++ function.js
@@ -0,0 +1,10 @@
+// A function is a block of organized, reusable code that used to perform a single, related actions
+
+
+function sum (a, b) {
+    return a+b
+}
+
+a = sum (3,4) ; // 7
+b = sum (5,6) ; //11
+console.log(a,b) // 7,11
